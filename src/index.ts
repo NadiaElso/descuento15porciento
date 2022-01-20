7eb94d7fa3744cafda981616627457a370c38a98
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-20 20:10:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+let PrecioUnitario = document.getElementById("PrecioUnitario");
+let Cantidad = document.getElementById("Cantidad");
+let Mes = document.getElementById("Mes");
+
+let boton = document.getElementById("Boton");
+
+function descuento() {
+  if (Number(Mes.value) === 10) {
+    let totalsindescuento =
+      Number(PrecioUnitario.value) * Number(Cantidad.value);
+    console.log(totalsindescuento);
+    let descuento: number;
+    descuento = Number(PrecioUnitario.value) * Number(Cantidad.value) * 0.15;
+    console.log(descuento);
+    let totalcondescuento = totalsindescuento - descuento;
+    console.log(totalcondescuento);
+  }
+}
+
+boton?.addEventListener("click", descuento);
